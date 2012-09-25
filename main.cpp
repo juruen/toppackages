a9a154489483084835bb3e0a1403d80780976345
Javier Uruen Val <juruen@warp.es>
2012-09-25 17:17:25 +0200

    Remove unused header

--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 #include "systemtap.h"
 #include "dllist.h"
 #include <iostream>
-#include <thread>
 #include <unistd.h>
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
 
 int main() {
   boost::asio::io_service io_service;
